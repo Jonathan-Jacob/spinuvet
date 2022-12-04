daf72df790d7d1f3589f8d4cf142e83f323f27d5
Jonathan Jacob <jjjscf@gmail.com>
2022-12-04 11:58:47 +0100

    wip: edit draft path

--- app/controllers/spina/admin/pages_controller.rb
+++ app/controllers/spina/admin/pages_controller.rb
@@ -54,7 +54,7 @@ module Spina
       def update
         Mobility.locale = @locale
         deserialzed_page_params = page_params
-        if !page_params[:json_attributes].nil? && page_params[:json_attributes].class = "String"
+        if !page_params[:json_attributes].nil? && page_params[:json_attributes].class == "String"
           deserialzed_page_params[:json_attributes] = JSON.parse(page_params[:json_attributes])
         end
         if @page.update(page_params) && PageDraft.create(view_template: @page.view_template.dup, json_attributes: @page.json_attributes.dup, spina_page_id: @page.id)
