21eabc9fea92a0cd38b55404a79ab7f4c4a088be
Jonathan Jacob <jjjscf@gmail.com>
2022-12-04 17:08:24 +0100

    wip: edit draft path

--- app/models/spina/page_draft.rb
+++ app/models/spina/page_draft.rb
@@ -3,4 +3,6 @@ class Spina::PageDraft < ApplicationRecord
   include AttrJson::NestedAttributes
 
   belongs_to :spina_page, class_name: "Spina::Page"
+
+  attr_json :json_attributes, AttrJson::Type::SpinaPartsModel.new, array: true
 end
