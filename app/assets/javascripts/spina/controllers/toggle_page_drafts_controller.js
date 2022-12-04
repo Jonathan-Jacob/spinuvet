cadf32650211be4058759dc56073565dd8ede5a3
Jonathan Jacob <jjjscf@gmail.com>
2022-12-04 12:15:07 +0100

    wip: edit draft path

--- app/assets/javascripts/spina/controllers/toggle_page_drafts_controller.js
+++ app/assets/javascripts/spina/controllers/toggle_page_drafts_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static get targets() {
-    return ['viewTemplate', 'content']
+    return ['viewTemplate', 'jsonAttributes']
   }
 
   toggle(event) {
