2f79a82eb672bfc7743834a2ffb93e416a8c78de
Jonathan Jacob <jjjscf@gmail.com>
2022-12-05 00:07:27 +0100

    wip: edit draft path

--- db/migrate/16_create_spina_page_drafts.rb
+++ db/migrate/16_create_spina_page_drafts.rb
@@ -3,6 +3,7 @@ class CreateSpinaPageDrafts < ActiveRecord::Migration[7.0]
     create_table :spina_page_drafts do |t|
       t.string :view_template, null: false
       t.jsonb :json_attributes
+      t.integer :version_id
       t.references :spina_page, null: false, foreign_key: true
       t.timestamps
     end
