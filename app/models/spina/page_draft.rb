a16f6da2ac42610db60ccf635f5649492dd79567
Jonathan Jacob <jjjscf@gmail.com>
2022-12-04 17:40:19 +0100

    wip: edit draft path

--- app/models/spina/page_draft.rb
+++ app/models/spina/page_draft.rb
@@ -6,8 +6,5 @@ module Spina
     include AttrJson::NestedAttributes
 
     belongs_to :spina_page, class_name: "Spina::Page"
-
-    attr_json :json_attributes, AttrJson::Type::SpinaPartsModel.new, array: true
-    attr_json_accepts_nested_attributes_for :json_attributes
   end
 end
