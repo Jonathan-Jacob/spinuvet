a00b662832d561b7c0c377d9108fdfb5c30bb899
Jonathan Jacob <jjjscf@gmail.com>
2022-12-06 00:10:26 +0100

    WIP: localize page drafts

--- app/models/spina/account.rb
+++ app/models/spina/account.rb
@@ -76,7 +76,8 @@ module Spina
             .first_or_create(title: page[:title])
             .update(view_template: page[:view_template], deletable: page[:deletable], version_counter: {I18n.default_locale => 1}, version_id: {I18n.default_locale => 1})
         spina_page = Page.where(name: page[:name]).first
-        PageDraft.where(spina_page_id: spina_page.id).first_or_create(view_template: spina_page.view_template.dup, locale: I18n.default_locale, version_id: 1, spina_page_id: spina_page.id)
+        local_content = JSON.parse(spina_page.json_attributes_before_type_cast)["#{I18n.default_locale}_content"]
+        PageDraft.where(spina_page_id: spina_page.id).first_or_create(view_template: spina_page.view_template.dup, json_attributes: local_content, locale: I18n.default_locale, version_id: 1, spina_page_id: spina_page.id)
       end
     end
 
