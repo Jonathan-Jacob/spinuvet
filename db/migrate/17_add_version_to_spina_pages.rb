fac4366b0a44442977152293eb5367003973364d
Jonathan Jacob <jjjscf@gmail.com>
2022-12-05 23:37:23 +0100

    WIP: localize page drafts

--- db/migrate/17_add_version_to_spina_pages.rb
+++ db/migrate/17_add_version_to_spina_pages.rb
@@ -1,6 +1,6 @@
 class AddVersionToSpinaPages < ActiveRecord::Migration[7.0]
   def change
-    add_column :spina_pages, :version_id, :jsonb, default: {I18n.locale => 1}
-    add_column :spina_pages, :version_counter, :jsonb, default: {I18n.locale => 1}
+    add_column :spina_pages, :version_id, :jsonb
+    add_column :spina_pages, :version_counter, :jsonb
   end
 end
