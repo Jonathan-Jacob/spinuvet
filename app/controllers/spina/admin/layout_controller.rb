598ed4903c22d74902e01b64b16fc5a9df798653
Jonathan Jacob <jjjscf@gmail.com>
2022-12-19 15:02:14 +0100

    reverting some stuff

--- app/controllers/spina/admin/layout_controller.rb
+++ app/controllers/spina/admin/layout_controller.rb
@@ -7,37 +7,34 @@ module Spina::Admin
     admin_section :content
 
     def edit
-      @part = params[:part]
     end
 
     def update
-      @part = params[:part]
       if @account.update(layout_params)
-        redirect_to spina.edit_admin_layout_path(locale: @locale, part: @part), flash: {success: t('spina.layout.saved')}
+        redirect_to spina.edit_admin_layout_path(locale: @locale), flash: {success: t("spina.layout.saved")}
       else
-        flash.now[:error] = t('spina.layout.couldnt_be_saved')
-        render partial: 'error', status: :unprocessable_entity
+        flash.now[:error] = t("spina.layout.couldnt_be_saved")
+        render partial: "error", status: :unprocessable_entity
       end
     end
 
     private
 
-      # Permit all attributes when editing your layout
-      def layout_params
-        params.permit(:part, :locale).merge(params.require(:account).permit!)
-      end
+    # Permit all attributes when editing your layout
+    def layout_params
+      params.require(:account).permit!
+    end
 
-      def set_breadcrumb
-        add_breadcrumb t('spina.layout.layout')
-      end
+    def set_breadcrumb
+      add_breadcrumb t("spina.layout.layout")
+    end
 
-      def set_account
-        @account = current_spina_account
-      end
+    def set_account
+      @account = current_spina_account
+    end
 
-      def set_locale
-        @locale = params[:locale] || I18n.default_locale
-      end
-
+    def set_locale
+      @locale = params[:locale] || I18n.default_locale
+    end
   end
 end
