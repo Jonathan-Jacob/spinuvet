e51d0f1c497b8486d013b90ec890f19ab6cd0695
Jonathan Jacob <jjjscf@gmail.com>
2022-12-28 23:23:45 +0100

    redo ingredient routing

--- app/models/spina/ingredient.rb
+++ app/models/spina/ingredient.rb
@@ -3,13 +3,13 @@ module Spina
     has_many :spina_product_ingredients
 
     def ingredient_name(locale)
-      return ingredient.json_attributes["#{locale}_content"]["name"] if ingredient.json_attributes["#{locale}_content"].present? && ingredient.json_attributes["#{locale}_content"]["name"].present?
+      return self.json_attributes["#{locale}_content"]["name"] if self.json_attributes["#{locale}_content"].present? && self.json_attributes["#{locale}_content"]["name"].present?
 
-      return ingredient.json_attributes["#{I18n.default_locale}_content"]["name"] if ingredient.json_attributes["#{I18n.default_locale}_content"].present? && ingredient.json_attributes["#{I18n.default_locale}_content"]["name"].present?
+      return self.json_attributes["#{I18n.default_locale}_content"]["name"] if self.json_attributes["#{I18n.default_locale}_content"].present? && self.json_attributes["#{I18n.default_locale}_content"]["name"].present?
 
-      return ingredient_name = ingredient.json_attributes.values.first["name"] if ingredient.json_attributes.values.first.present? && ingredient.json_attributes.values.first["name"].present?
+      return self.json_attributes.values.first["name"] if self.json_attributes.values.first.present? && self.json_attributes.values.first["name"].present?
 
-      "#{I18n.t('spina.ingredients.ingredient')} #{ingredient.id}"
+      "#{I18n.t('spina.ingredients.ingredient')} #{self.id}"
     end
   end
 end
