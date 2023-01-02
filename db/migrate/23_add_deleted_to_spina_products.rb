e2b5d873eecae3bb8537de9ba5180c934f101709
Jonathan Jacob <jjjscf@gmail.com>
2023-01-02 22:45:24 +0100

    add deleted to products

--- db/migrate/23_add_deleted_to_spina_products.rb
+++ db/migrate/23_add_deleted_to_spina_products.rb
@@ -1,4 +1,4 @@
-class AddDeletedToProducts < ActiveRecord::Migration[7.0]
+class AddDeletedToSpinaProducts < ActiveRecord::Migration[7.0]
   def change
     add_column :products, :deleted, :boolean, default: false
   end
