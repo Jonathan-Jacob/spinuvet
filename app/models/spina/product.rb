6cc14b2f6e52ffe1aa90cd4fc95c8f649fecb665
Jonathan Jacob <jjjscf@gmail.com>
2023-01-02 23:05:52 +0100

    create products

--- app/models/spina/product.rb
+++ app/models/spina/product.rb
@@ -1,5 +1,15 @@
 module Spina
   class Product < ApplicationRecord
-    has_many :spina_product_ingredients
+    has_many :spina_product_products
+
+    def product_name(locale)
+      return self.json_attributes["#{locale}_content"]["name"] if self.json_attributes["#{locale}_content"].present? && self.json_attributes["#{locale}_content"]["name"].present?
+
+      return self.json_attributes["#{I18n.default_locale}_content"]["name"] if self.json_attributes["#{I18n.default_locale}_content"].present? && self.json_attributes["#{I18n.default_locale}_content"]["name"].present?
+
+      return self.json_attributes.values.first["name"] if self.json_attributes.values.first.present? && self.json_attributes.values.first["name"].present?
+
+      "#{I18n.t('spina.products.product')} #{self.id}"
+    end
   end
 end
