025bd48ca299cff1c7023cf47e74cade41c088fc
Jonathan Jacob <jjjscf@gmail.com>
2023-01-03 13:50:58 +0100

    create productingredients

--- db/migrate/20_create_spina_product_ingredients.rb
+++ db/migrate/20_create_spina_product_ingredients.rb
@@ -1,8 +1,8 @@
 class CreateSpinaProductIngredients < ActiveRecord::Migration[7.0]
   def change
     create_table :spina_product_ingredients do |t|
-      t.references :spina_product, null: false, foreign_key: true
-      t.references :spina_ingredient, null: false, foreign_key: true
+      t.references :product, null: false, foreign_key: {to_table: :spina_products}
+      t.references :ingredient, null: false, foreign_key: {to_table: :spina_ingredients}
 
       t.timestamps
     end
