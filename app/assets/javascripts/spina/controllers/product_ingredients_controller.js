3fca7921bea02c07c6081888bc5c0d992ec6a048
Jonathan Jacob <jjjscf@gmail.com>
2023-01-07 23:12:44 +0100

    create productingredients

--- app/assets/javascripts/spina/controllers/product_ingredients_controller.js
+++ app/assets/javascripts/spina/controllers/product_ingredients_controller.js
@@ -23,7 +23,7 @@ export default class extends Controller {
             <path fill-rule="evenodd" d="M4.5 2A1.5 1.5 0 003 3.5v13A1.5 1.5 0 004.5 18h11a1.5 1.5 0 001.5-1.5V7.621a1.5 1.5 0 00-.44-1.06l-4.12-4.122A1.5 1.5 0 0011.378 2H4.5zm5 5a3 3 0 101.524 5.585l1.196 1.195a.75.75 0 101.06-1.06l-1.195-1.196A3 3 0 009.5 7z" clip-rule="evenodd"></path>
           </svg>
         </a>
-        <button name="button" type="button">
+        <button name="button" type="button" data-action="product-ingredients#remove" data-ingredient-id="${event.currentTarget.dataset.ingredientId}" data-ingredient-name="${event.currentTarget.dataset.ingredientName}">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="w-5 h-5">
             <path fill-rule="evenodd" d="M4.28 3.22a.75.75 0 00-1.06 1.06L8.94 10l-5.72 5.72a.75.75 0 101.06 1.06L10 11.06l5.72 5.72a.75.75 0 101.06-1.06L11.06 10l5.72-5.72a.75.75 0 00-1.06-1.06L10 8.94 4.28 3.22z" clip-rule="evenodd"></path>
           </svg>
