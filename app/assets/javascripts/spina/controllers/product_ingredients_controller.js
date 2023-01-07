4f696693779c967dee00a3e3d6dd017779a70b5a
Jonathan Jacob <jjjscf@gmail.com>
2023-01-07 22:23:54 +0100

    create productingredients

--- app/assets/javascripts/spina/controllers/product_ingredients_controller.js
+++ app/assets/javascripts/spina/controllers/product_ingredients_controller.js
@@ -2,15 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static get targets() {
-    return ["selectedContainer"];
+    return ["allIngredientsContainer", "selectedContainer"];
   }
 
   add(event) {
     const tempNewNode = document.createElement('div');
     tempNewNode.innerHTML = `<div class="flex justify-between items-center sm:ml-2 p-2 my-2 bg-white rounded border border-gray-300 shadow-sn">
       <div class="flex items-center gap-2 overflow-hidden">
-        <button name="button" type="button">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="w-5 h-5 cursor-grab">
+        <button name="button" type="button" class="cursor-grab">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="w-5 h-5">
             <path fill-rule="evenodd" d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z" clip-rule="evenodd"></path>
           </svg>
         </button>
@@ -49,6 +49,11 @@ export default class extends Controller {
       </div>
     </div>`;
     event.currentTarget.parentNode.parentNode.parentNode.replaceChild(tempOldNode.firstChild, event.currentTarget.parentNode.parentNode);
+  }
+
+  remove(event) {
+    event.currentTarget.parentNode.parentNode.remove();
+
   }
 
 }
