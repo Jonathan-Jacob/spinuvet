ae94a6cdc84669004f67d75a14a127abf934ce00
Jonathan Jacob <jjjscf@gmail.com>
2023-01-07 17:45:29 +0100

    create productingredients

--- app/assets/javascripts/spina/controllers/product_ingredients_controller.js
+++ app/assets/javascripts/spina/controllers/product_ingredients_controller.js
@@ -48,7 +48,7 @@ export default class extends Controller {
         </button>
       </div>
     </div>`;
-    this.event.currentTarget.parentNode.innerHTML = tempOldNode.firstChild;
+    this.event.currentTarget.parentNode.parentNode.innerHTML = tempOldNode.firstChild;
   }
 
 }
