dc64f7f864fb44be237a8fc5cf3a94fc70df6432
Jonathan Jacob <jjjscf@gmail.com>
2023-01-08 23:02:18 +0100

    create productingredients

--- app/assets/javascripts/spina/controllers/trix_controller.js
+++ app/assets/javascripts/spina/controllers/trix_controller.js
@@ -18,7 +18,7 @@ export default class extends Controller {
         this.imageFieldsTarget.classList.add("hidden")
       }
     }.bind(this));
-
+    this.pizzaTarget.firstChild.classList.add('animate-spin');
     setTimeout(() => this.pizzaTarget.remove(), 1000);
   }
 
