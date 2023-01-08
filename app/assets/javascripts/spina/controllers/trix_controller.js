52df0136c1ee2e78fa442338cc150d5a8a8eefa0
Jonathan Jacob <jjjscf@gmail.com>
2023-01-08 22:51:41 +0100

    create productingredients

--- app/assets/javascripts/spina/controllers/trix_controller.js
+++ app/assets/javascripts/spina/controllers/trix_controller.js
@@ -19,7 +19,7 @@ export default class extends Controller {
       }
     }.bind(this));
 
-    setTimeout(this.pizzaTarget.remove(), 1000);
+    setTimeout(this.pizzaTarget.remove(), 2000);
   }
 
   insertEmbeddable(html) {
