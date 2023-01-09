b72739319ac40a169ec7d5b5e0750adfbde205b2
Jonathan Jacob <jjjscf@gmail.com>
2023-01-09 16:31:27 +0100

    create productingredients

--- app/models/spina/ingredient_draft.rb
+++ app/models/spina/ingredient_draft.rb
@@ -1,5 +1,17 @@
 module Spina
   class IngredientDraft < ApplicationRecord
     belongs_to :ingredient
+
+    def ingredient_name(locale)
+      return self.json_attributes["#{locale}_content"]["name"] if self.json_attributes["#{locale}_content"].present? && self.json_attributes["#{locale}_content"]["name"].present?
+
+      return self.json_attributes["#{I18n.default_locale}_content"]["name"] if self.json_attributes["#{I18n.default_locale}_content"].present? && self.json_attributes["#{I18n.default_locale}_content"]["name"].present?
+
+      Spina.locales.each do |spina_locale|
+        return self.json_attributes["#{spina_locale}_content"]["name"] if self.json_attributes["#{spina_locale}_content"].present? && self.json_attributes["#{spina_locale}_content"]["name"].present?
+      end
+
+      "#{I18n.t('spina.ingredients.ingredient')} #{self.id}"
+    end
   end
 end
