9dfb9590fa6ebc954f5d6eb4fddb292e19b7084d
Jonathan Jacob <jjjscf@gmail.com>
2023-01-10 09:29:13 +0100

    create productingredients

--- app/controllers/spina/admin/products_controller.rb
+++ app/controllers/spina/admin/products_controller.rb
@@ -80,7 +80,7 @@ module Spina::Admin
       end
 
       if @product.save
-         Spina::ProductDraft.create(product: @product, json_attributes: @product.json_attributes.dup, version_id: @product.version_id) unless product_params[:active_product_draft].present?
+         Spina::ProductDraft.create(product_id: @product.id, json_attributes: @product.json_attributes.dup, version_id: @product.version_id) unless product_params[:active_product_draft].present?
         redirect_to admin_products_path(locale: @locale), flash: {success: t("spina.product.saved")}
       else
         flash.now[:error] = t("spina.product.couldnt_be_saved")
