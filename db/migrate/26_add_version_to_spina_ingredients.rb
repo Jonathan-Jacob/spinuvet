65d3f6b0c940db772e918e36b1fadfd18a444369
Jonathan Jacob <jjjscf@gmail.com>
2023-01-10 09:21:14 +0100

    create productingredients

--- db/migrate/26_add_version_to_spina_ingredients.rb
+++ db/migrate/26_add_version_to_spina_ingredients.rb
@@ -1,6 +1,6 @@
 class AddVersionToSpinaIngredients < ActiveRecord::Migration[7.0]
   def change
-    add_column :spina_ingredients, :version_id, :integer
-    add_column :spina_ingredients, :version_counter, :integer
+    add_column :spina_ingredients, :version_id, :integer, default: 1
+    add_column :spina_ingredients, :version_counter, :integer, default: 1
   end
 end
--- db/migrate/28_add_version_to_spina_products.rb
+++ db/migrate/28_add_version_to_spina_products.rb
@@ -1,6 +1,6 @@
 class AddVersionToSpinaProducts < ActiveRecord::Migration[7.0]
   def change
-    add_column :spina_products, :version_id, :integer
-    add_column :spina_products, :version_counter, :integer
+    add_column :spina_products, :version_id, :integer, default: 1
+    add_column :spina_products, :version_counter, :integer, default: 1
   end
 end
